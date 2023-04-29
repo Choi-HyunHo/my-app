70f200beea188eea93ad2317e56efb3358f8a7b3
최현호 <x7788@naver.com>
2023-04-29 16:05:26 +0900

    feat : 렌더링 연습

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,23 @@
+import { getProducts } from "@/service/products";
+import Link from "next/link";
+
 const Products = () => {
-    return <div>이곳은 Products 페이지 입니다</div>;
+    // 서버에 있는 제품의 리스트를 읽어옴
+    const products = getProducts();
+
+    return (
+        <>
+            <div>이곳은 Products 페이지 입니다</div>
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        <Link href={`/products/${product}`}>{product}</Link>
+                    </li>
+                ))}
+            </ul>
+            ;
+        </>
+    );
 };
 
 export default Products;
