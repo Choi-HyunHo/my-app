ececbc7aeaf632650c95f2895ea4ce089ba52d16
최현호 <x7788@naver.com>
2023-05-06 02:33:37 +0900

    study : loading.js

---
+++ src/app/posts/loading.tsx
@@ -0,0 +1,3 @@
+export default function PostLoading() {
+    return <span>로딩 중 입니다</span>;
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 // export const revalidate = 0;
 
 const Posts = async () => {
-    const res = await fetch("https://api.adviceslip.com/advice", {
-        next: { revalidate: 0 },
-        // cache : 'no-store'
-    });
+    const res = await fetch("https://api.adviceslip.com/advice", {});
     const data = await res.json();
     const randomText = data.slip.advice;
 
