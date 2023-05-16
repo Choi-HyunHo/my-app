f7c3f7a4f2cf21e9ace8b37fce82e1cc6acc1990
Choi-HyunHo <x7788@naver.com>
2023-05-16 14:32:02 +0900

    study : fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Counter from "../components/Counter";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-    return (
-        <main className="flex min-h-84 flex-col items-center justify-between p-24">
-            <Counter />
-        </main>
-    );
+	return (
+		<main className="flex min-h-84 flex-col items-center justify-between p-24">
+			<Counter />
+		</main>
+	);
 }
--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,22 +1,34 @@
 import style from "../layout.module.css";
+import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 
 export const metadata = {
-    title: "여기는 Posts",
-    description: "Posts",
+	title: "여기는 Posts",
+	description: "Posts",
 };
 
+const inter = Inter({ subsets: ["latin"] });
+const sans = Open_Sans({ subsets: ["latin"] });
+
+// 나눔 고딕은 variable fonts 가 아닙니다!
+const nanum = Nanum_Gothic({
+	weight: "800",
+	subsets: ["latin"],
+});
+
 export default function PostsLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <>
-            <div className={style.header}>
-                <a>apple</a>
-                <a>banana</a>
-            </div>
-            {children}
-        </>
-    );
+	return (
+		<main className={nanum.className}>
+			<div className={style.header}>
+				<a className={sans.className}>apple</a>
+				<a className={inter.className}>banana</a>
+			</div>
+			{children}
+		</main>
+	);
 }
