bd9f0c224bdc8c9c4b9e426d40b253ea864c3494
Choi-HyunHo <x7788@naver.com>
2023-05-16 15:11:00 +0900

    study : redirect

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/posts/login", // 사용자 접근 경로
+				destination: "/posts", // 리다이렉트 시킬 경로
+				permanent: true, // 캐싱 관련 여부
+			}, // 위와 같은 형식으로 여러 개 만들 수 있습니다.
+		];
+	},
 };
 
 module.exports = nextConfig;
